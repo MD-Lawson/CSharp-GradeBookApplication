2d5810a3ba84b0934cd848f47009fedf2bdbf3bc
matthewl <matthew.lawson1@vodafone.com>
2019-04-02 15:04:26 +0100

    Completed Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,5 +10,42 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Must have at least 5 students");
+                
+            }
+
+            var threshold = Students.Count * 0.2;
+            var count = 0;
+
+            foreach(Student Student in Students)
+            {
+                if(averageGrade >= Student.AverageGrade)
+                {
+                    count++;
+                }
+            }
+
+            if(count >= threshold * 5)
+            {
+                return 'A';
+            }else if(count >= threshold * 4)
+            {
+                return 'B';
+            }
+            else if (count >= threshold * 3)
+            {
+                return 'C';
+            }
+            else if (count >= threshold * 2)
+            {
+                return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
