506a4e689120e8b98bfc771424281a3783c7cdea
matthewl <matthew.lawson1@vodafone.com>
2019-04-02 11:25:37 +0100

    Added using GradeBook.Enums to RankedGradeBook. Minor Formatting

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,11 +1,12 @@
-﻿
+﻿using GradeBook.Enums;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name): base(name)
         {
-            Type = Enums.GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
         }
 
     }
